72e671c42de7fa749c3aeb3cfc6b2ee45b7c2b20
DESKTOP-DD2BR4M\Front-End Developer <frontendercode@gmail.com>
2018-09-12 13:02:49 +0300

    v.0.17
    Сверстал desktop версию шаблона, без мобильного меню + исправить title

--- app/libs/jquery/common.js
+++ app/libs/jquery/common.js
@@ -60,19 +60,47 @@ $('#comand_carousel').owlCarousel({
 });
 
 
+$('#cor').owlCarousel({
+  // center: true,
+  loop: true,
+  nav: true,
+  dots:true,
+//  navText: ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+  responsive:{
+    0:{
+      items: 1,
+    },
+    768:{
+      items: 2,
+    },
+    990:{
+      items: 2,
+    }
+  },
+  onInitialized: coverFlowEfx,
+  onTranslate: coverFlowEfx,
+});
 
-
-
-
-
-
-
-
-// код подключения плагина Vertical-Horizontal-Tabs
-  // $('#verticalTab').jqTabs();
+function coverFlowEfx(e){
+  // if ($('.owl-dots')) {
+  //   $('.owl-dots').remove();
+  // }
+  idx = e.item.index;
+  $('.owl-item.big').removeClass('big');
+  $('.owl-item.medium').removeClass('medium');
+  $('.owl-item.mdright').removeClass('mdright');
+  $('.owl-item.mdleft').removeClass('mdleft');
+  $('.owl-item.smallRight').removeClass('smallRight');
+  $('.owl-item.smallLeft').removeClass('smallLeft');
+  $('.owl-item').eq(idx -1).addClass('medium mdleft');
+  $('.owl-item').eq(idx).addClass('big');
+  $('.owl-item').eq(idx + 1).addClass('medium mdright');
+  $('.owl-item').eq(idx + 2).addClass('smallRight');
+  $('.owl-item').eq(idx - 2).addClass('smallLeft');
+}
 
 // всплывающие окна обратной связи позвонить мне
-$("a[href='#politics'], a[href='#call-backtimer'], a[href='#call-back'], a[href='#call-back4s'], a[href='#call-back6s'], a[href='#call-back8s'], a[href='#call-back10s']").magnificPopup ({
+$("a[href='#call-backtimer']").magnificPopup ({
 	mainClass:'mfp-fade',
 	removalDelay:400,
 	type:'inline'
@@ -80,26 +108,12 @@ $("a[href='#politics'], a[href='#call-backtimer'], a[href='#call-back'], a[href=
 
 
 /*чтобы в формах был индивидуальный заголовок */
-$("a[href='#politics'], a[href='#call-backtimer'], a[href='#call-back'], a[href='#call-back4s'], a[href='#call-back6s'], a[href='#call-back8s'], a[href='#call-back10s']").click(function(){
+$("a[href='#call-backtimer']").click(function(){
 	var dataForm = $(this).data("form");
 	var dataText = $(this).data("text");
 	$(".forms-call h4").text(dataText);
 	$(".forms-call [name=admin-data]").val(dataForm);
 });
-
-
-$('input').iCheck({
-	checkboxClass: 'icheckbox_minimal',
-	radioClass: 'iradio_minimal',
-	radioClass: 'iradio_minimal',
-    increaseArea: '30%' // optional
-  });
-
-
-
-
-
-
 
 //Ajax push mesege http://api.jquery.com/jquery.ajax/
 
